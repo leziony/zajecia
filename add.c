de0af5afaaa5acab32532dc40db16536320f585f
leziony <you@example.com>
2020-01-13 19:35:33 +0100

    dodawanie plikow i przygotowane kodu pod kalkulator.

---
+++ add.c
@@ -0,0 +1,14 @@
+void adding
+{
+int n;
+scanf ("%d",&n);
+int x[n];
+int i=0;
+int sum=0;
+for (i=0,i<n,i++)
+{
+	scanf("%d",x[i]);
+	sum=sum+x[i];
+}
+printf("%d",sum);
+}
--- main.c
+++ main.c
@@ -5,6 +5,16 @@ int main ()
 {
 	int i=0;
 	printf("%d means it's good!",i);
+	scanf("%d",&i);
+	switch (i)
+	{
+		case 1: i++;
+		printf("%d",i);
+		break;
+		case 2:i--;
+		printf("%d",i);
+		break;
+	}
 	return 0;
 
 }
